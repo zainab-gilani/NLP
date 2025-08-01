4cba2a1a1de37f8049a820ad9317eae80798c9bc
zainab-gilani <zainab.gilani@yahoo.com>
2025-08-01 18:58:21 +0500

    Fixed regex to ignore interests

--- mini_tests.py
+++ mini_tests.py
@@ -2,8 +2,7 @@ import re
 
 input_str = "i got a in maths, b in physics, im interested in med"
 
-# Pattern: "A in maths", "B in physics"
-pattern = r"(A\*|A|B|C|D|E|U)\s+in\s+([a-zA-Z\s]+?)(?:,|$)"
+pattern = r"\b(A\*|A|B|C|D|E|U)\b\s+in\s+([a-zA-Z\s]+?)(?:,|$)"
 
 matches = re.findall(pattern, input_str, re.IGNORECASE)
 
